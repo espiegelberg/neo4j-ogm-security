1b205d078d8d87b5abfea6a1c295a46e2dd9e422
Eric Spiegelberg <eric@miletwentyfour.com>
2016-01-17 12:05:34 -0600

    Provide some basic test coverage.

--- src/test/java/com/mile24/example/neo4j/ogm/typeconversion/SecureStringPropetyConverterTest.java
+++ src/test/java/com/mile24/example/neo4j/ogm/typeconversion/SecureStringPropetyConverterTest.java
@@ -7,20 +7,29 @@ import com.mile24.example.neo4j.ogm.typeconversion.SecureStringPropertyConverter
 
 public class SecureStringPropetyConverterTest {
 
+	private SecureStringPropertyConverter converter = new SecureStringPropertyConverter();
+
+	public static final String SAMPLE_TEXT = "For authorized eyes only";
+	
 	@Test
 	public void validateSecurity() {
 
-		String plaintext = "For authorized eyes only";
-		SecureStringPropertyConverter converter = new SecureStringPropertyConverter();
-		
-		String encryptedValue = converter.toGraphProperty(plaintext);
-		Assert.assertNotEquals(plaintext, encryptedValue);
+		String encryptedValue = converter.toGraphProperty(SAMPLE_TEXT);
+		Assert.assertNotEquals(SAMPLE_TEXT, encryptedValue);
 		
 		String decryptedValue = converter.toEntityAttribute(encryptedValue);
 		Assert.assertNotEquals(encryptedValue, decryptedValue);
 		
-		Assert.assertEquals(decryptedValue, plaintext);
+		Assert.assertEquals(decryptedValue, SAMPLE_TEXT);
 
+		String value1 = "value1";
+		String plaintext1 = converter.toEntityAttribute(value1);
+		Assert.assertEquals(value1, plaintext1);
+		
+		String value2 = "ENC(Y+7MdqpeAELqdNqi5+viz+lnApN+qflbpMROaGqQIIlEWcyz6HOfYw==)";
+		String plaintext2 = converter.toEntityAttribute(value2);
+		Assert.assertEquals(SAMPLE_TEXT, plaintext2);
+		
 	}
 
 }
