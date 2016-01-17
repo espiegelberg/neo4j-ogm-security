7fde733a24f4178fee98d2dbe7bd003bcacd333c
Eric Spiegelberg <eric@miletwentyfour.com>
2016-01-17 12:15:25 -0600

    Change the default encryption password. Add ability to detected encrypted vs unencrypted properties.

--- src/main/java/com/mile24/example/neo4j/ogm/typeconversion/SecureStringPropertyConverter.java
+++ src/main/java/com/mile24/example/neo4j/ogm/typeconversion/SecureStringPropertyConverter.java
@@ -18,48 +18,64 @@ import org.neo4j.ogm.typeconversion.AttributeConverter;
  */
 public class SecureStringPropertyConverter implements AttributeConverter<String, String> {
 
+	public static final String ENCRYPTED_TOKEN_START = "ENC(";
+	public static final String ENCRYPTED_TOKEN_END = ")";
+	
 	private BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
 	
 	public SecureStringPropertyConverter() {
 
-		textEncryptor.setPassword("myPassword");
+		textEncryptor.setPassword("my-password-change-me");
 
 	}
 	
 	/**
-	 * Encrypt the value before it is persisted to Neo4j.
+	 * Encrypt the value before it is persisted to the database.
 	 */
 	public String toGraphProperty(String value) {
 
+		StringBuilder sb = new StringBuilder(ENCRYPTED_TOKEN_START);
+		
 		String encryptedText = textEncryptor.encrypt(value);
-
-		return encryptedText;
+		sb.append(encryptedText);
+		sb.append(ENCRYPTED_TOKEN_END);
+		
+		return sb.toString();
 		
 	}
 
 	/**
-	 * Decrypt the value retrieved from Neo4j.
+	 * Decrypt the database value to the value set on the object model.
 	 */
 	public String toEntityAttribute(String value) {
 
 		String result = value;
 		
-//		boolean encrypted = isEncrypted(value);
-//		if (encrypted) {
-//			result = value.substring(4, result.length() - 1);
-//			//result = result.substring(0, );
-//		}
+		boolean encrypted = isEncrypted(value);
 		
-		result = textEncryptor.decrypt(result);
+		if (encrypted) {
+			result = value.substring(4, result.length() - 1);
+			result = textEncryptor.decrypt(result);
+		}
 		
 		return result;
 	}
 
+	/**
+	 * Determine if a value is encrypted or not.
+	 * 
+	 * This is a convenience to help facilitate scenarios where 
+	 * <code>SecureStringPropertyConverter</code> is applied to properties who's values 
+	 * are not all encrypted within the database, for example.
+	 * 
+	 * @param value
+	 * @return True if the value is determined to be encrypted, otherwise false.
+	 */
 	protected boolean isEncrypted(String value) {
 		
 		boolean result = false;
 		
-		if (value.startsWith("ENC(") && value.endsWith(")")) {
+		if (value.startsWith(ENCRYPTED_TOKEN_START) && value.endsWith(ENCRYPTED_TOKEN_END)) {
 			result = true;
 		}
 		
